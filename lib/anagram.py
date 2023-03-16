a8f242bbe65f484a8d36b90b2d446883f0ac4f50
sdbarlow <sethdbarlow@gmail.com>
2023-03-16 12:55:04 -0600

    all tests pass

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    def match(self, wlist):
+        new_array = []
+        for words in wlist:
+           if sorted([letter for letter in self.word]) == sorted([letter for letter in words]):
+                new_array.append(words)
+        return new_array
+            
+
+seth = Anagram("listen")
+print(seth.match(['enlists', 'google', 'inlets', 'silent', 'banana']))
+
